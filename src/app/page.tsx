400988e89b2403008d8402eefd0b84c57903c5d8
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-06-10 19:11:22 +0500

    add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,19 +23,37 @@ export default function Page() {
               className="w-24 h-auto sm:w-40 md:w-48 lg:w-48 -translate-y-2 p-2 pt-4"
             />
             <ul className="flex gap-6 ml-auto flex-1 md:flex-initial mx-4 items-center">
-              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">Home</li>
-              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">Services</li>
-              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">Case Studies</li>
-              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">Career</li>
-              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">Blog</li>
+              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">
+                Home
+              </li>
+              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">
+                Services
+              </li>
+              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">
+                Case Studies
+              </li>
+              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">
+                Career
+              </li>
+              <li className="text-white font-normal text-xs md:text-lg hover:text-yellow-400">
+                Blog
+              </li>
             </ul>
           </header>
         </section>
       </main>
-      <section className="relative bg-black py-24 z-10 border-t border-white">
-        <h1 className="text-7xl text-white text-center">Services Section</h1>
+      <section className="relative bg-black p-4 font-extrabold py-24 z-10 border-t border-white">
+        <h1 className="text-7xl text-transparent stroke-white bg-clip-text bg-transparent px-4 py-2">
+          Services Section
+        </h1>
       </section>
-      <section className="relative bg-black py-24 z-10 border-t border-white">
+      <section className="text-7xl text-transparent stroke-white bg-clip-text bg-transparent px-4 py-2">
+        <h1 className="text-7xl text-white text-center">Our Work</h1>
+      </section>
+      <section className="text-7xl text-transparent stroke-white bg-clip-text bg-transparent px-4 py-2">
+        <h1 className="text-7xl text-white text-center">Testimonial</h1>
+      </section>
+      <section className="text-7xl text-transparent stroke-white bg-clip-text bg-transparent px-4 py-2">
         <h1 className="text-7xl text-white text-center">Footer section</h1>
       </section>
     </>
--- src/components/cursor.tsx
+++ src/components/cursor.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
@@ -7,67 +7,71 @@ import { useRef } from "react";
 import clamp from "@/util/clamp";
 
 export default function Cursor() {
-
-
   const ball = useRef<HTMLDivElement>(null);
   const cursor = useRef<HTMLDivElement>(null);
 
-  useGSAP(()=> {
-    
-    if(ball.current === null || cursor.current === null) return;
+  useGSAP(() => {
+    if (ball.current === null || cursor.current === null) return;
 
-    let mouse = { x: 0, y: 0 }; 
-    let pos = { x: 0, y: 0 }; 
-    let ratio = .08; // .1
-    
-	  let threshold = 20
-	  let maxBlur = 5
-    let lastX
-    let lastY
+    let mouse = { x: 0, y: 0 };
+    let pos = { x: 0, y: 0 };
+    let ratio = 0.08; // .1
+
+    let threshold = 20;
+    let maxBlur = 5;
+    let lastX;
+    let lastY;
 
     // tween lite
-    gsap.to(ball.current,  { xPercent: -50, yPercent: -50 });
+    gsap.to(ball.current, { xPercent: -50, yPercent: -50 });
     gsap.ticker.fps(60);
-    
-    const updatePositions = ()=> {
-      lastX =  pos.x
-			lastY =  pos.y
+
+    const updatePositions = () => {
+      lastX = pos.x;
+      lastY = pos.y;
 
       pos.x += (mouse.x - pos.x) * ratio;
       pos.y += (mouse.y - pos.y) * ratio;
 
-      const deltaX = pos.x - lastX
-		  const deltaY = pos.y - lastY
+      const deltaX = pos.x - lastX;
+      const deltaY = pos.y - lastY;
 
-      const theta = Math.atan2(deltaY, deltaX) * (180 / Math.PI)
-      
-      const vX = clamp(Math.abs(deltaX) / threshold, 0, 1)
-      const vY = clamp(Math.abs(deltaY) / threshold, 0, 1)
-      const velocity = (vX + vY) * 0.4
+      const theta = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+
+      const vX = clamp(Math.abs(deltaX) / threshold, 0, 1);
+      const vY = clamp(Math.abs(deltaY) / threshold, 0, 1);
+      const velocity = (vX + vY) * 0.4;
 
       // tween lite
-      gsap.to(ball.current, { x: pos.x, y: pos.y, scaleX: `1`, rotation: theta.toFixed(),'-webkit-filter':'blur('+(velocity * maxBlur)+'px)'});
-      gsap.to(cursor.current, { x: mouse.x, y: mouse.y});
+      gsap.to(ball.current, {
+        x: pos.x,
+        y: pos.y,
+        scaleX: `1`,
+        rotation: theta.toFixed(),
+        "-webkit-filter": "blur(" + velocity * maxBlur + "px)",
+      });
+      gsap.to(cursor.current, { x: mouse.x, y: mouse.y });
     };
 
-    
     gsap.ticker.add(updatePositions);
 
-    const mouseMove = (e: any)=> {
+    const mouseMove = (e: any) => {
       mouse.x = e.clientX;
       mouse.y = e.clientY;
     };
 
     document.addEventListener("mousemove", mouseMove);
-    
-    return ()=> {
+
+    return () => {
       gsap.ticker.remove(updatePositions);
       document.removeEventListener("mousemove", mouseMove);
     };
-  }, [cursor, ball]); 
+  }, [cursor, ball]);
 
-  return <>
-    <div id="cursor" ref={cursor}></div>
-    <div id="ball" ref={ball}></div>
-  </>
+  return (
+    <>
+      <div id="cursor" ref={cursor}></div>
+      <div id="ball" ref={ball}></div>
+    </>
+  );
 }
